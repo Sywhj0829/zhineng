edd972425688c3074b9ee7c84185543fb79e3cba
syw <2827876565@qq.com>
2022-10-26 10:59:28 +0800

    first

--- src/main/java/com/ncyteng/zhineng/ZhinengApplication.java
+++ src/main/java/com/ncyteng/zhineng/ZhinengApplication.java
@@ -3,13 +3,20 @@ package com.ncyteng.zhineng;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
 @MapperScan("com.ncyteng.zhineng.dao")
-public class ZhinengApplication {
+public class ZhinengApplication extends SpringBootServletInitializer {
 
     public static void main(String[] args) {
         SpringApplication.run(ZhinengApplication.class, args);
+    }
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+        // 注意这里要指向原先用main方法执行的Application启动类
+        return builder.sources(ZhinengApplication.class);
     }
 
 }
